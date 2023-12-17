2e99b3e319e4a92ab93d136ac7dbafd2e03bff4d
karamsinghus <karamsing468@gmail.com>
2023-12-17 21:41:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import TodoList from "./Component/TodoList";
+
+function App() {
+  return <TodoList />;
+}
+
+export default App;
---
+++ src/Component/TodoList.js
@@ -0,0 +1,270 @@
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaRegListAlt } from "react-icons/fa";
+import { FaRegSquarePlus } from "react-icons/fa6";
+import { IoMdDoneAll } from "react-icons/io";
+import { MdDelete, MdEditNote } from "react-icons/md";
+import "./Style.css";
+
+const TodoList = () => {
+  // State variables
+  const [tasks, setTasks] = useState([]); // Holds the list of tasks
+  const [inputValue, setInputValue] = useState(""); // Holds the value of the input field
+  const [filter, setFilter] = useState("all"); // Holds the current filter type
+  const [isLoading, setIsLoading] = useState(true); // Indicates whether the data is being loaded
+  const [editTaskId, setEditTaskId] = useState(null); // Holds the ID of the task being edited
+
+  // Fetch initial data
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  // Fetch todos from an API
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=4"
+      );
+      const todos = await response.json();
+      setTasks(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.log("Error fetching todos:", error);
+      setIsLoading(false);
+    }
+  };
+
+  // Handle input change
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Add a new task
+  const handleAddTask = async () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
+
+    const newTask = {
+      title: inputValue,
+      completed: false,
+    };
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos",
+        {
+          method: "POST",
+          body: JSON.stringify(newTask),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const addedTask = await response.json();
+      setTasks((prevTasks) => [...prevTasks, addedTask]);
+      setInputValue("");
+      toast.success("Task added successfully");
+    } catch (error) {
+      console.log("Error adding task:", error);
+      toast.error("Error adding task");
+    }
+  };
+
+  // Handle checkbox change for a task
+  const handleTaskCheckboxChange = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  // Delete a task
+  const handleDeleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+    toast.success("Task deleted successfully");
+  };
+
+  // Edit a task
+  const handleEditTask = (taskId) => {
+    setEditTaskId(taskId);
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    setInputValue(taskToEdit.title);
+  };
+
+  // Update a task
+  const handleUpdateTask = async () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
+
+    const updatedTask = {
+      title: inputValue,
+      completed: false,
+    };
+
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${editTaskId}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(updatedTask),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const updatedTaskData = await response.json();
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === editTaskId
+            ? { ...task, title: updatedTaskData.title }
+            : task
+        )
+      );
+      setInputValue("");
+      setEditTaskId(null);
+      toast.success("Task updated successfully");
+    } catch (error) {
+      console.log("Error updating task:", error);
+      toast.error("Error updating task");
+    }
+  };
+
+  // Mark all tasks as completed
+  const handleCompleteAll = () => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => ({ ...task, completed: true }))
+    );
+  };
+
+  // Clear completed tasks
+  const handleClearCompleted = () => {
+    setTasks((prevTasks) => prevTasks.filter((task) => !task.completed));
+  };
+
+  // Handle filter change
+  const handleFilterChange = (filterType) => {
+    setFilter(filterType);
+  };
+
+  // Filter tasks based on the selected filter
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "all") {
+      return true;
+    } else if (filter === "completed") {
+      return task.completed;
+    } else if (filter === "uncompleted") {
+      return !task.completed;
+    }
+    return true;
+  });
+
+  // Display loading message while data is being fetched
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Render the todo list
+  return (
+    <div className="container">
+      <ToastContainer />
+      <div className="todo-app">
+        <div className="heading">
+          <FaRegListAlt size={34} />
+          <h2>Todo List</h2>
+        </div>
+        <div className="row">
+          <FaRegSquarePlus size={24} />
+          <input
+            type="text"
+            className="add-task"
+            id="add"
+            placeholder="Add your todo"
+            autoFocus
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button
+            id="btn"
+            onClick={editTaskId ? handleUpdateTask : handleAddTask}
+          >
+            {editTaskId ? "Update" : "Add"}
+          </button>
+        </div>
+
+        <div className="mid">
+          <button id="complete-all" onClick={handleCompleteAll}>
+            <IoMdDoneAll size={20} />
+            Mark All Completed
+          </button>
+          <button id="clear-all" onClick={handleClearCompleted}>
+            <MdDelete size={20} />
+            Delete All Completed
+          </button>
+        </div>
+
+        <ul id="list">
+          {filteredTasks.map((task) => (
+            <li key={task.id}>
+              <input
+                type="checkbox"
+                id={`task-${task.id}`}
+                data-id={task.id}
+                className="custom-checkbox"
+                checked={task.completed}
+                onChange={() => handleTaskCheckboxChange(task.id)}
+              />
+              <label htmlFor={`task-${task.id}`}>{task.title}</label>
+              <div>
+                <MdEditNote
+                size={20}
+                  data-id={task.id}
+                  onClick={() => handleEditTask(task.id)}
+                />
+                <MdDelete size={20} data-id={task.id} onClick={() => handleDeleteTask(task.id)} />
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <div className="filters">
+          <div className="dropdown">
+            <button className="dropbtn">Filter</button>
+            <ul className="dropdown-content">
+              <li id="all" onClick={() => handleFilterChange("all")}>
+                All
+              </li>
+              <li id="rem" onClick={() => handleFilterChange("uncompleted")}>
+                Uncompleted
+              </li>
+              <li id="com" onClick={() => handleFilterChange("completed")}>
+                Completed
+              </li>
+            </ul>
+          </div>
+
+          <div className="completed-task">
+            <p>
+              Completed:{" "}
+              <span id="c-count">
+                {tasks.filter((task) => task.completed).length}
+              </span>
+            </p>
+          </div>
+          <div className="remaining-task">
+            <p>
+              <span id="total-tasks">
+                Total Tasks: <span id="tasks-counter">{tasks.length}</span>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
